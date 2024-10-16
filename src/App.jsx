b0e91137ba6ca413017561ba95070a0c77805011
raiyan1999j <62192284+raiyan1999j@users.noreply.github.com>
2024-10-16 15:23:37 +0600

    primary setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./Components/Navbar/Navbar";
+
+export default function App(){
+  return(
+    <>
+      <header>
+        <Navbar/>
+      </header>
+
+      <main></main>
+
+      <footer></footer>
+    </>
+  )
+}
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import Banner from "./Banner";
+import NavigationBar from "./NavigationBar";
+
+export default function Navbar(){
+    return(
+        <>
+            <NavigationBar/>
+            <Banner/>
+        </>
+    )
+}
